d516a32625e2c541aad723322be9bf79dcc487da
nehasuryawanshi993-hub <nehasuryawanshi993@gmail.com>
2025-12-25 23:34:02 +0530

    Create L_0_Mars.cpp

---
+++ L_0_Mars.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'marsExploration' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int marsExploration(string s) {
+int n=s.size();
+int k=0;
+for(int i=0;i+2<n;i=i+3){
+    
+        if(s[i]!='S'){
+            k++;
+        }
+        if(s[i+1]!='O'){
+            k++;
+        }
+        if(s[i+2]!='S'){
+        k++;
+        }
+    
+}
+return k;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = marsExploration(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
